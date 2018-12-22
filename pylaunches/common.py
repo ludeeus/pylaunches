b38b4d27707588dfe824226555fe84651df12269
ludeeus <joasoe@gmail.com>
2018-12-22 19:40:04 +0100

    iso

--- pylaunches/common.py
+++ pylaunches/common.py
@@ -44,6 +44,4 @@ class CommonFunctions():
         """Convert to timestamp."""
         from datetime import datetime, timezone
         unix_timestamp = int(source)
-        utc_time = datetime.fromtimestamp(unix_timestamp, timezone.utc)
-        local_time = utc_time.astimezone()
-        return local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)").isoformat()
+        return datetime.fromtimestamp(unix_timestamp).isoformat()
