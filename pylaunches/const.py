92ca4d3640e48a4ae9d68eba45aee790de786b3d
ludeeus <joasoe@gmail.com>
2023-04-16 20:00:04 +0000

    Add optional dev argument to use the development API

--- pylaunches/const.py
+++ pylaunches/const.py
@@ -1,6 +1,7 @@
 """Constants."""
 
 BASE_URL = "https://ll.thespacedevs.com"
+DEV_BASE_URL = "https://lldev.thespacedevs.com"
 DEFAULT_API_VERSION = "2.2.0"
 
 DOCUMENTATION = "https://thespacedevs.com/llapi"
--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,17 +1,24 @@
 import pytest
 from pylaunches import PyLaunches
-from pylaunches.const import DEFAULT_API_VERSION
+from pylaunches.const import BASE_URL, DEFAULT_API_VERSION, DEV_BASE_URL
 
 
 @pytest.mark.asyncio
 async def test_default_api_version():
     """Test default api version."""
     async with PyLaunches() as client:
-        assert client._base_url == f"https://ll.thespacedevs.com/{DEFAULT_API_VERSION}"
+        assert client._base_url == f"{BASE_URL}/{DEFAULT_API_VERSION}"
 
 
 @pytest.mark.asyncio
 async def test_custom_api_version():
     """Test custom api version."""
     async with PyLaunches(api_version="0.1.0") as client:
-        assert client._base_url == "https://ll.thespacedevs.com/0.1.0"
+        assert client._base_url == f"{BASE_URL}/0.1.0"
+
+
+@pytest.mark.asyncio
+async def test_dev_url():
+    """Test custom api version."""
+    async with PyLaunches(dev=True) as client:
+        assert client._base_url == f"{DEV_BASE_URL}/{DEFAULT_API_VERSION}"
