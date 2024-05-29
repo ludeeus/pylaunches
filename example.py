796afbb43e9fc16a4155f89ab8417a2b6bfef788
ludeeus <joasoe@gmail.com>
2024-05-29 07:57:58 +0000

    Move the API handling into the PyLaunches class

--- example.py
+++ example.py
@@ -9,8 +9,7 @@ from pylaunches import PyLaunches, PyLaunchesError
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(message)s",
     handlers=[logging.StreamHandler()],
 )
 
@@ -21,9 +20,9 @@ async def example():
     """Example usage of pylaunches."""
     async with PyLaunches(token=os.environ.get("LAUNCH_TOKEN"), dev=True) as client:
         try:
-            events = await client.event_upcoming(filters={"limit": 1, "program": "1"})
+            events = await client.event_upcoming(filters={"limit": 1})
             for event in events:
-                log.info("%s", event["name"])
+                log.info("%s: %s", event["date"], event["name"])
         except PyLaunchesError as exception:
             log.exception(exception)
 
