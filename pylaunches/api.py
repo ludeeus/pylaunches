3d74fd74532caf55ddc329ea0ee6df0f027a2f91
ludeeus <joasoe@gmail.com>
2024-05-29 06:29:40 +0000

    lint

--- pylaunches/api.py
+++ pylaunches/api.py
@@ -74,11 +74,11 @@ class PyLaunches:
     ) -> StarshipResponse:
         """Get upcoming launch information for starship."""
         response: StarshipResponse = await call_api(
-                self.session,
-                f"{self._base_url}/dashboard/starship/",
-                self.token,
-                params=filters,
-            )
+            self.session,
+            f"{self._base_url}/dashboard/starship/",
+            self.token,
+            params=filters,
+        )
         if not response.get("previous", {}).get("launches"):
             raise PyLaunchesError("No starship data.")
         return response
