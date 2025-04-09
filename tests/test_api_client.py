26e884e2c42f1de94a2022c5989e6576f95c35fc
ludeeus <joasoe@proton.me>
2025-04-09 06:35:16 +0000

    Remvoe ability to use a custom API version

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,24 +1,17 @@
 import pytest
 from pylaunches import PyLaunches
-from pylaunches.const import BASE_URL, DEFAULT_API_VERSION, DEV_BASE_URL
+from pylaunches.const import API_VERSION, BASE_URL, DEV_BASE_URL
 
 
 @pytest.mark.asyncio
 async def test_default_api_version():
-    """Test default api version."""
+    """Test default URL."""
     async with PyLaunches() as client:
-        assert client._base_url == f"{BASE_URL}/{DEFAULT_API_VERSION}"
-
-
-@pytest.mark.asyncio
-async def test_custom_api_version():
-    """Test custom api version."""
-    async with PyLaunches(api_version="0.1.0") as client:
-        assert client._base_url == f"{BASE_URL}/0.1.0"
+        assert client._base_url == f"{BASE_URL}/{API_VERSION}"
 
 
 @pytest.mark.asyncio
 async def test_dev_url():
-    """Test custom api version."""
+    """Test dev URL."""
     async with PyLaunches(dev=True) as client:
-        assert client._base_url == f"{DEV_BASE_URL}/{DEFAULT_API_VERSION}"
+        assert client._base_url == f"{DEV_BASE_URL}/{API_VERSION}"
